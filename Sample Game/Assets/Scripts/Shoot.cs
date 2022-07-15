298a58379f18b6f1804605f8423812ae6159c9f4
BradleyGallavin <93383136+BradleyGallavin@users.noreply.github.com>
2022-07-15 14:02:11 +0100

    Removed commented out code in Shoot.cs and tidied up if statement at line 25 of Shoot.cs

--- Sample Game/Assets/Scripts/Shoot.cs
+++ Sample Game/Assets/Scripts/Shoot.cs
@@ -17,29 +17,12 @@ public class Shoot : MonoBehaviour
     }
     public void Fire()
     {
-        /*
-        RaycastHit HitInfo;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out HitInfo, 100.0f))
-        {
-            Transform objectHit = HitInfo.transform;
-            Debug.Log("Transform pos"+ this.transform.position);
-            Debug.Log("End Point" + HitInfo.point);
-            Debug.DrawLine(this.transform.position, this.transform.forward, Color.red, 2, false);
-            if (HitInfo.rigidbody != null)
-
-                objectHit.GetComponent<Rigidbody>().AddForceAtPosition(this.transform.position, HitInfo.point, ForceMode.Impulse);
-            
-        }
-        */
-
         RaycastHit HitInfo;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out HitInfo, 100.0f))
         {
             Transform objectHit = HitInfo.transform;
             Debug.DrawLine(cam.transform.position, HitInfo.point, Color.red, 2, false);
-            if (HitInfo.rigidbody != null)
-                
-            objectHit.GetComponent<Rigidbody>().AddForceAtPosition(cam.transform.forward * 1000, HitInfo.point, ForceMode.Impulse);
+            if (HitInfo.rigidbody != null) objectHit.GetComponent<Rigidbody>().AddForceAtPosition(cam.transform.forward * 1000, HitInfo.point, ForceMode.Impulse);
         }
     }
 
