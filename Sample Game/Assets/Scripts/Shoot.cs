d7c3526e84fb576913a22aa90b27c2e9a95dabc9
BradleyGallavin <93383136+BradleyGallavin@users.noreply.github.com>
2022-07-17 15:42:34 +0100

    Tidied up shoot.cs a bit

--- Sample Game/Assets/Scripts/Shoot.cs
+++ Sample Game/Assets/Scripts/Shoot.cs
@@ -9,12 +9,26 @@ public class Shoot : MonoBehaviour
     private PlayerInput playerInput;
     private PlayerInput.OnFootActions onFoot;
     private RaycastHit Hitinfo;
+
     Transform gun;
+    bool aiming = false;
+    Vector3 gunPosition;
+    Vector3 gunRotation;
+
+    Vector3 aimPosition;
+    Vector3 aimRotation;
+
     public void Start()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
         gun = cam.GetComponent<Transform>().GetChild(0);
+
+        gunPosition = gun.localPosition;
+        gunRotation = gun.localEulerAngles;
+
+        aimPosition = new Vector3(0f, -0.58f, 0.85f);
+        aimRotation = new Vector3(0f, 180f, 0f);
     }
     public void Fire()
     {
@@ -29,13 +43,21 @@ public class Shoot : MonoBehaviour
 
     public void Aim()
     {
-        // Hard coded position of the gun when aiming down sights.
-        gun.localPosition = new Vector3(0f, -0.58f, 0.85f);
-        gun.localEulerAngles = new Vector3(0f, 180f, 0f);
-    }
+        if (!aiming)
+        {
+            aiming = true;
+            cam.fieldOfView = 60f;
+            // Hard coded position of the gun when aiming down sights.
 
-    void FixedUpdate()
-    {
-
+            gun.localPosition = aimPosition;
+            gun.localEulerAngles = aimRotation;
+        }
+        else
+        {
+            aiming = false;
+            cam.fieldOfView = 90f;
+            gun.localPosition = gunPosition;
+            gun.localEulerAngles = gunRotation;
+        }
     }
 }
