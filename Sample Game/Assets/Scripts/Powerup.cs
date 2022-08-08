024150545721e5bb5b7a461689bca6453c0cd7e4
BradleyGallavin <93383136+BradleyGallavin@users.noreply.github.com>
2022-08-09 00:05:25 +0100

    Added a powerup script that can be attached to objects and a further house model (still work in progress)

---
+++ Sample Game/Assets/Scripts/Powerup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+
+    public Collider pickupTrigger;
+    public enum PowerupTypes { SpeedUpgrade, JumpUpgrade };
+    public PowerupTypes type;
+
+
+    private void OnTriggerEnter(Collider pickupTrigger)
+    {
+        if (pickupTrigger.tag == "Player")
+        {
+            Destroy(gameObject);
+
+            GameObject player = pickupTrigger.gameObject;
+            PlayerMotor motor = player.GetComponent<PlayerMotor>();
+
+            if (type == PowerupTypes.SpeedUpgrade)
+            {
+                motor.speed += 10f;
+                motor.runSpeed += 10f;
+            }
+            else if (type == PowerupTypes.JumpUpgrade)
+            {
+                motor.jumpHeight += 10f;
+            }
+        }
+    }
+}
